0f381bef958dd08b0b7abd614a97b5fab666789a
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-05-19 09:47:27 +0530

    To print the series of ABCDE.

---
+++ series of ABCDE.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   char i,j;
+	for(i='A';i<='E';i++)
+	{
+		for(j='A';j<='E';j++)
+		{
+			cout<< j ;
+		}
+		cout<<"\n";
+	}
+	return 0;	
+}
